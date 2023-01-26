9a0bcb6ac99fd66bde2f8dc43a5b06aefde4272f
yokozhem <kozhemyakova.elena@gmail.com>
2023-01-26 17:28:55 -0500

    1 and 3 done

---
+++ 000/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// С клавиатуры вводятся два числа. Найти и вывести на экран их сумму
+
+int a,b,sum;
+Console.WriteLine("Введите a: ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+b=Convert.ToInt32(Console.ReadLine());
+sum=a+b;
+//1. Конкатенация (скливание) строк
+System.Console.WriteLine(a+"+"+b.ToString()+"="+Convert.ToString(sum));
+//2. Форматированный вывод
+System.Console.WriteLine("{0:F2}+{1:E5}={2:G4}",a,b,sum);
+//3. Интерполяция строк
+System.Console.WriteLine($"{a}+{b}={sum}");
+
